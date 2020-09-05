b885b66a89ce0371ba1a6139ba764c7c2562fda8
xiaoqiang1234567 <30685925+xiaoqiang1234567@users.noreply.github.com>
2020-09-05 16:02:44 +0800

    Revert "When for traverses the device list, send invite and sleep to other threads to update the list, and the conflicting program exits"
    This reverts commit 82f1569f2a623942aeafeb59d4bf39a72a89cb8f.

--- trunk/src/app/srs_app_gb28181_sip.cpp
+++ trunk/src/app/srs_app_gb28181_sip.cpp
@@ -163,8 +163,7 @@ srs_error_t SrsGb28181SipSession::do_cycle()
         if (_register_status == SrsGb28181SipSessionRegisterOk &&
             _alive_status == SrsGb28181SipSessionAliveOk)
         {
-            std::list<std::string> auto_play_list;
-           
+               
             std::map<std::string, SrsGb28181Device*>::iterator it;
             for (it = _device_list.begin(); it != _device_list.end(); it++) {
                 SrsGb28181Device *device = it->second;
@@ -192,15 +191,8 @@ srs_error_t SrsGb28181SipSession::do_cycle()
                 //offline or already invite device does not need to send invite
                 if (device->device_status != "ON" || 
                     device->invite_status != SrsGb28181SipSessionUnkonw) continue;
-                
-                auto_play_list.push_back(chid);
-            }//end for (it) 
 
-            //auto send sip invite and create stream chennal
-            while(auto_play_list.size() > 0){
-                std::string chid = auto_play_list.front();
-                auto_play_list.pop_front();
-        
+
                 SrsGb28181StreamChannel ch;
             
                 ch.set_channel_id(_session_id + "@" + chid);
@@ -234,7 +226,7 @@ srs_error_t SrsGb28181SipSession::do_cycle()
             
                 srs_trace("gb28181: %s clients device=%s send invite code=%d", 
                     _session_id.c_str(), chid.c_str(), code);
-            }//end while (auto_play_list.size())
+            }//end for (it)
         }//end if (config)
 
         if (_register_status == SrsGb28181SipSessionRegisterOk &&
